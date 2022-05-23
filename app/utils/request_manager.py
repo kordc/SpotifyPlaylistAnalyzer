0e67c19dabc05db200f20990e4793000313e16fd
kordc <karol.cyganik@wp.pl>
2022-05-23 14:12:39 +0200

    add data types in classes

--- app/utils/request_manager.py
+++ app/utils/request_manager.py
@@ -1,19 +1,19 @@
 from spotifyData import getdata
 
 class RequestManager():
-    def __init__(self, columns):
+    def __init__(self, columns: list):
         self.requests = {}
         self.num_of_requests = 0
         self.columns = columns
 
-    def add_request(self, phrase):
+    def add_request(self, phrase: str):
         self.requests[self.num_of_requests] = {"label": phrase, "value": self.num_of_requests}
         self.num_of_requests += 1
 
     def get_options(self):
         return list(self.requests.values())
 
-    def add_data(self, rows, outcome):
+    def add_data(self, rows: list, outcome):
         if isinstance(outcome, getdata.Track):
             rows.insert(0, self.get_instance(outcome))
         elif isinstance(outcome, getdata.Playlist):
@@ -22,15 +22,14 @@ class RequestManager():
         
         return rows
 
-    def get_instance(self, row):
+    def get_instance(self, row: getdata.Track):
         features = row.getFeatures(wanted_features=self.columns)
         features["id"] = self.num_of_requests
 
         return features
 
-    def remove_request(self, rows, request_id):
+    def remove_request(self, rows: list, request_id: str):
         if request_id is None: return rows
-        print(request_id)
         self.requests.pop(request_id)
         return [row for row in rows if row["id"] != request_id]
         
