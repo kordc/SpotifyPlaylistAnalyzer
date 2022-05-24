2730bb0db271b1454d9a18b7aeaaa86acedfdfac
Karol Cyganik <72265576+kordc@users.noreply.github.com>
2022-05-24 13:52:50 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from spotifyData.getdata import DatasetCreator
-from plots.plots import Plots
-
-if __name__ == "__main__":
-    dataset = DatasetCreator()
-    plots = Plots()
-    pl = dataset.getTopPlaylist("PL")
-    fig = plots.radarPlot(pl)
-    fig.show()
